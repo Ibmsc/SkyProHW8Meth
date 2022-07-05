17b763556e129bc04c7e40392e0edf323d1194a2
ibmsc <Ibm.sc1409@gmail.com>
2022-07-05 22:41:38 +0300

    completed 3 task

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,41 @@ public class Main {
         return;
         }
     }
-
     public static void task2СheckingOS(int clientOS){
-
        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-
         } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         return;}
-
+    public static void deliveryDistance (int distance) {
+        int day = 1;
+        if(distance <=20) {
+            System.out.println("Потребуется дней " + day);
+        }else{
+            if (distance >= 20 && distance < 60)
+                System.out.println("Потребуется дней " + day*2);
+            else
+            if(distance >= 60 && distance <100)
+                System.out.println("Потребуется дней " + day * 3);
+            else
+            if(distance >=100)
+                System.out.println("Потребуется более 3 дней.");
+        }
+    }
     public static void task2YearOfRelease(int yearOS) {
-
         if (yearOS < 2022){
             System.out.println("Установите облегченную версию приложения по ссылке");
-
     }else {
             System.out.println("Установите обычную версию приложения по ссылке");
             return;}}
-
             public static void main (String[]args){
                 System.out.println("Задача №1!");
-
                 task1(2020);
-
                 System.out.println("Задача №2!");
-
                 task2YearOfRelease(2022);
                 task2СheckingOS(1);
-
-
+                System.out.println("Задача №3!");
+                deliveryDistance(123);
             }
         }
