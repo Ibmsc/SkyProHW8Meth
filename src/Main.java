cdfbd7e72280bbf3e1e0813c602025991c800eef
ibmsc <Ibm.sc1409@gmail.com>
2022-07-05 18:32:27 +0300

    completed 1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
+    public static void task1(int year) {
+        // Здесь пишем код первого задания
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(year + " год является високосным!");
+
+            return;
+        }else {  System.out.println(year + " год не является високосным!");
+
+        return;
+        }
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача №1!");
+
+        task1(2022);
+
     }
 }
