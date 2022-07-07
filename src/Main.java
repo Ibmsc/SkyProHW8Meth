12488e9502fead7074c5d9636480e1bdccb24d61
ibmsc <Ibm.sc1409@gmail.com>
2022-07-07 14:42:41 +0300

    Исправил ошибки,доработал код2

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,36 @@ public class Main {
         // Здесь пишем код первого задания
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
             System.out.println(year + " год является високосным!");
-
         }else {
             System.out.println(year + " год не является високосным!");
-
         }
     }
-    public static void СheckingOS(int clientOS, int clientDeviceYear){
-
+    public static void checkingOS(int clientOS, int clientDeviceYear){
         if (clientOS == 0 ){
             if(clientDeviceYear < 2022){
                 System.out.println("Установите Lite версию приложения для iOS по ссылке");
             }else {
                 System.out.println("Установите обычную версию приложения да iOS по ссылке");
             }
+        } else if (clientOS == 1 && clientDeviceYear < 2022) {
+
+            System.out.println("Установите Lite версию приложения для Android по ссылке");
         } else {
-            if (clientDeviceYear < 2022) {
-                System.out.println("Установите Lite версию приложения для Android по ссылке");
-            } else {
-                System.out.println("Установите обычную версию приложения для Android по ссылке");
-            }
+            System.out.println("Установите обычную версию приложения для Android по ссылке");
         }
     }
-
     public static void deliveryDistance (int distance) {
         int deliveryDay = 1;
 
         if (distance > 20){
             deliveryDay++;
         }
-        if ( distance > 60) {
+        if ( distance > 60 ) {
             deliveryDay++;
+        }
+        if ( distance > 100) {
+            System.out.println("Нет доставки");
+            return ;
         }
         System.out.println("Потребуется дней : " + deliveryDay);
     }
@@ -43,9 +42,9 @@ public class Main {
 
                 System.out.println("Задача №2!");
 
-                СheckingOS(0,2022);
+                checkingOS(0,2022);
 
                 System.out.println("Задача №3!");
-                deliveryDistance(10);
+                deliveryDistance(100);
         }
 }
